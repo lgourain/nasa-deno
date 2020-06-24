a89a3f2516c76dded13f6ccd42a9b435483b163d
Louis Gourain <lgourain@wanadev.fr>
2020-06-24 17:48:59 +0200

    add logging middleware

--- mod.ts
+++ mod.ts
@@ -3,6 +3,10 @@ import { Application } from "https://deno.land/x/oak@v5.0.0/mod.ts";
 const app = new Application();
 const PORT = 8000;
 
+app.use(async (ctx, next) => {
+    await next();
+    console.log(`${ctx.request.method} ${ctx.request.url}`)
+});
 
 app.use((ctx) => {
     ctx.response.body = "Mission Control API";
