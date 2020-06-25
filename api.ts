5e46695d76f104c66d23c119d83949e9b79ed2c8
Louis Gourain <lgourain@wanadev.fr>
2020-06-25 18:00:00 +0200

    setup oak router

---
+++ api.ts
@@ -0,0 +1,9 @@
+import { Router } from "https://deno.land/x/oak@v5.0.0/mod.ts";
+
+const router = new Router();
+
+router.get("/", (ctx) => {
+    ctx.response.body = "Mission Control API";
+});
+
+export default router;
