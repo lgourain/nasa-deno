f533aadeb9e2daf6856e887dfbc910c33dbcca45
Louis Gourain <lgourain@wanadev.fr>
2021-01-31 20:00:38 +0100

    add dependencies file for local use

--- deps.ts
+++ deps.ts
@@ -1,6 +1,5 @@
 // Standard library dependencies
 export * as log from "https://deno.land/std@0.85.0/log/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.85.0/testing/asserts.ts"
 export { join } from "https://deno.land/std@0.85.0/path/mod.ts";
 export { BufReader } from "https://deno.land/std@0.85.0/io/bufio.ts";
 export { parse } from "https://deno.land/std@0.85.0/encoding/csv.ts";
--- models/planets.test.ts
+++ models/planets.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "../deps.ts";
+import { assertEquals } from "../test_deps.ts";
 import { filterHabitablePlanets } from "./planets.ts";
 
 const HABITABLE_PLANET = {
